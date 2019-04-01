670ef9782ee7147f8672e75ac0a47fbc98f8e0f7
leneveuT <leneveu.thibaut@gmail.com>
2019-04-01 22:25:26 +0200

    Ajout des headers manquants

--- cheval/create.php
+++ cheval/create.php
@@ -1,6 +1,7 @@
 <?php
 
 // required headers
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
